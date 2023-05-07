9d486a3d10cffe2abc11d7da6383f6a601a47c98
Avasarala Sai Kiran <58164887+asaikiran1999@users.noreply.github.com>
2023-05-07 11:50:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def main():
 			    for review in soup.find_all('div', class_='review-text-content'):
 			        reviews = reviews.get_text().strip()
 			    for i in range(0,len(reviews)):
-				reviews_list.append(reviews[i].get_text())
+						reviews_list.append(reviews[i].get_text())
 			st.text('webscraping completed')
 			st.text('labeling good and bad started')      
                         #for review in soup.find_all('div', class_='review-text-content'):
@@ -63,11 +63,11 @@ def main():
 			 #reviews_list.append(reviews[i].get_text())
     
     # Check if the review contains images or videos
-                            for i in reviews_list:
-			if 'img' not in i and 'video' not in i:
-				sentiment = []
-				for i in range(len(reviews_list))
-				sentiment.append(int(model.predict(vec.transform([reviews_list[i]]))))
+          for i in reviews_list:
+				if 'img' not in i and 'video' not in i:
+					sentiment = []
+				for i in range(len(reviews_list)):
+					sentiment.append(int(model.predict(vec.transform([reviews_list[i]]))))
 
 			
 	        
