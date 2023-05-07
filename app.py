af154a3bd638a0d53f7b7e275723d9a14b0cf531
Avasarala Sai Kiran <58164887+asaikiran1999@users.noreply.github.com>
2023-05-07 15:48:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def main():
                 response = requests.get(url, headers=headers)
                 soup = BeautifulSoup(response.text, 'html.parser')
                 reviews = []
-                for review in soup.find_all('div', {'data-hook': 'review'}):
+                for review in soup.find_all('div', {'class': 'review-text'}):
                     review_text = review.find('span', {'data-hook': 'review-body'}).text.strip()
                     rating = float(review.find('i', {'data-hook': 'review-star-rating'}).text.replace('out of 5 stars', '').strip())
                     reviews.append(review_text)
