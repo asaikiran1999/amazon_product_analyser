49bce6ce0c7a4f80c93ef799c320daaeb252566c
Avasarala Sai Kiran <58164887+asaikiran1999@users.noreply.github.com>
2023-05-07 12:17:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,8 @@ def main():
 			url_cut = url
 			reviews_list = []
 			st.text('webscraping the reviews')
-			for review in soup.find_all('div', class_='review-text-content'):reviews = reviews.get_text().strip()for i in range(0,len(reviews)):reviews_list.append(reviews[i].get_text())
+			for review in soup.find_all('div', class_='review-text-content'):reviews = reviews.get_text().strip()
+				for i in range(0,len(reviews)):reviews_list.append(reviews[i].get_text())
 			st.text('webscraping completed')
 			st.text('labeling good and bad started')
 			for i in reviews_list:
