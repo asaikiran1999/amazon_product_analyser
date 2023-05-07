d478c60c8129e87e0a87e39a3ccf5873cbcec4ad
Avasarala Sai Kiran <58164887+asaikiran1999@users.noreply.github.com>
2023-05-07 12:34:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def main():
 			reviews_list = []
 			st.text('webscraping the reviews')
 			for review in soup.find_all('div', class_='review-text-content'):
-				review = review.get_text().strip()
+				reviews = review.get_text().strip()
 			for i in range(0,len(reviews)):reviews_list.append(reviews[i].get_text())
 			st.text('webscraping completed')
 			st.text('labeling good and bad started')
